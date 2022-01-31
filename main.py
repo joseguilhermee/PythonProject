1554dc391c5988c677a31a51aa50389b09b7e1eb
Guilherme <joseguilhermeeneto@gmail.com>
2022-01-31 09:18:20 -0300

    Sistema de Adicionar e Buscar

--- main.py
+++ main.py
@@ -2,7 +2,42 @@
 
 AGENDA = {}
 
-def primeiro_metodo():
-    
+AGENDA['Guilherme'] = {
+    'telefone': '99999999',
+    'email': 'guilherme@gmail.com',
+}
+AGENDA['Guilhermee'] = {
+    'telefone': '99999999',
+    'email': 'guilherme@gmail.com',
+}
 
-primeiro_metodo()
+def layout():
+    print('____________________________________')
+
+def search_contact(contact):
+    print("Telefone:", AGENDA[contact]['telefone'])
+    print("Email:", AGENDA[contact]['email'])
+
+def show_contacts():
+    for contact in AGENDA:
+        layout()
+        print("NOME:", contact)
+        search_contact(contact)
+        layout()
+
+def search(contact):
+    layout()
+    print('Nome:', contact)
+    search_contact(contact)
+    layout()
+
+def add_contact(contato, telefone, email):
+    AGENDA[contato] = {
+        'telefone': telefone,
+        'email': email,
+    }
+    print('contato adicionado com sucesso')
+
+add_contact('joao', '7777777', 'testando@gmail.com')
+
+show_contacts()
